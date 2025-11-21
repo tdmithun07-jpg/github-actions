071a392f60ccec6d40c6e0fdd5c849d5cc5080c3
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-11-21 17:33:56 +0530

    vnet added

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg" {
   name = var.resource_group_name
     location = var.location
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-2111"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
